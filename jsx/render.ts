89fd96e4657b30411a5079d8e53048ffe841906a
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-05-16 23:16:32 -0400

    cleanup render function

--- jsx/render.ts
+++ jsx/render.ts
@@ -1,25 +1,65 @@
-const docType: string = '<!doctype html>';
+interface VirtualDom {
+  nodeName: string;
+  attributes?: object;
+  children?: Array<object | string>;
+}
 
-export const render = (vdom: any) => {
-  const attributes: string = Object.keys(vdom.attributes || {}).reduce((attrs, key) => {
-    return `${attrs} ${key}="${vdom.attributes[key]}"`;
-  }, '');
+const DOC_TYPE: string = '<!doctype html>';
+const SELF_CLOSING_ELEMENTS = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+];
 
-  const includedDocType: string = vdom.nodeName === 'html' ? docType : '';
+function generateAttributes(attributes?: object): string {
+  const attr: any = attributes || {};
+  return Object.keys(attr).map((key) => {
+    const value: string = attr[key] || '';
+    return `${key}="${value}"`;
+  }).join('');
+}
 
-  const openingTag: string = `${includedDocType}<${vdom.nodeName}${attributes}>`;
-  const closingTag: string = `</${vdom.nodeName}>`;
+function createVoidElement(
+  { nodeName, attributes }: { nodeName: string; attributes?: object },
+): string {
+  const attr: string = generateAttributes(attributes);
+  return `<${nodeName} ${attr}/>`;
+}
 
-  const children = vdom.children.map((child: any) => {
-    if (typeof child === 'string') {
-      return child;
-    }
-    else {
-      return render(child);
-    }
+function createElement(
+  { nodeName, attributes, children = [] }: {
+    nodeName: string;
+    attributes?: object;
+    children?: Array<object | string>;
+  },
+): string {
+  const attr: string = generateAttributes(attributes);
+  const includedDocType: string = nodeName === 'html' ? DOC_TYPE : '';
+  const openingTag: string = `${includedDocType}<${nodeName} ${attr}>`;
+  const closingTag: string = `</${nodeName}>`;
+
+  const childNodes = children.map((child: any) => {
+    return typeof child === 'string' ? child : render(child);
   });
 
-  const node = [openingTag, ...children, closingTag];
+  const node = [openingTag, ...childNodes, closingTag];
 
   return node.join('');
-};
+}
+
+export function render(vdom: VirtualDom) {
+  const isSelfClosing: boolean = SELF_CLOSING_ELEMENTS.includes(vdom.nodeName);
+
+  return isSelfClosing ? createVoidElement(vdom) : createElement(vdom);
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
-import { Status } from "https://deno.land/std/http/http_status.ts";
-import { ServerRequest } from "https://deno.land/std/http/server.ts";
+import { Status } from 'https://deno.land/std/http/http_status.ts';
+import { ServerRequest } from 'https://deno.land/std/http/server.ts';
 import { h, render } from '../jsx/index.ts';
 
 const template = (
@@ -10,12 +10,16 @@ const template = (
     </head>
     <body>
       <h1>Welcome to JSX 🦕🦖</h1>
-      <img src="https://cdn.britannica.com/s:800x1000/83/211283-131-3DB62136/Tyrannosaurus-Rex-Struthiomimus-dinosaurs.jpg" alt="" />
+      <img
+        src="https://cdn.britannica.com/s:800x1000/83/211283-131-3DB62136/Tyrannosaurus-Rex-Struthiomimus-dinosaurs.jpg"
+        alt=""
+      />
     </body>
   </html>
 );
 
-export default (req: ServerRequest) => req.respond({
-  status: Status.OK,
-  body: render(template),
-});
+export default (req: ServerRequest) =>
+  req.respond({
+    status: Status.OK,
+    body: render(template),
+  });
